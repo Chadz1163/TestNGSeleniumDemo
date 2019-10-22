c93488ea7ee29fd97d2e399d95ed6210108d6112
shidao.zhang <shidao.zhang@ruisdata.com>
2019-10-22 21:04:00 +0800

    a

--- src/test/java/UITest/utils/Constants.java
+++ src/test/java/UITest/utils/Constants.java
@@ -9,6 +9,6 @@ public class Constants {
     public static final String EXCELFILEPATH = "/Demo.xlsx";
     public static final String BROWSER = "chrome";
     public static final String DRIVER = "webdriver.chrome.driver";
-    public static final String DRIVERURL = "/chromedriver.exe";
+    public static final String DRIVERURL = "/workspace/chromedriver.exe";
     public static final String DEMO_SHEET = "login";
 }
