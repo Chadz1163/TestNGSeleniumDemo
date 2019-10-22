29bede7c123bee0436cc05d43ce0d4c42c62da91
shidao.zhang <shidao.zhang@ruisdata.com>
2019-10-22 20:45:16 +0800

    修改了driverUrl

--- src/test/java/UITest/utils/Constants.java
+++ src/test/java/UITest/utils/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
     public static final String EXCELFILEPATH = "/Demo.xlsx";
     public static final String BROWSER = "chrome";
     public static final String DRIVER = "webdriver.chrome.driver";
-    public static final String DRIVERURL = "/chromedriver.exe";
+    //public static final String DRIVERURL = "/chromedriver.exe";
+    public static final String DRIVERURL = "/root/workspace/chromedriver.exe";
     public static final String DEMO_SHEET = "login";
 }
